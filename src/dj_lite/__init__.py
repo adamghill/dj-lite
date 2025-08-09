f304102bcc0199624603a20b52b99bc729bf2228
adamghill <adamghill@yahoo.com>
2025-08-09 17:30:11 -0400

    Add kwargs for pragma values. Add `temp_store=MEMORY` pragma by default.

--- src/dj_lite/__init__.py
+++ src/dj_lite/__init__.py
@@ -1,47 +1,21 @@
-from typeguard import typechecked
-from enum import StrEnum
-from pathlib import Path
+from dj_lite.configurator import sqlite_config
+from dj_lite.enums import Synchronous, TempStore, JournalMode, TransactionMode
 
 
-SQLITE_INIT_COMMAND = """
-PRAGMA journal_mode=WAL;
+SQLITE_INIT_COMMAND = """PRAGMA journal_mode=WAL;
 PRAGMA synchronous=NORMAL;
+PRAGMA temp_store=MEMORY;
 PRAGMA mmap_size=134217728;
 PRAGMA journal_size_limit=27103364;
 PRAGMA cache_size=2000;
 """
 
 
-class TransactionMode(StrEnum):
-    DEFERRED = "DEFERRED"
-    IMMEDIATE = "IMMEDIATE"
-    EXCLUSIVE = "EXCLUSIVE"
-
-
-@typechecked
-def sqlite_config(
-    base_dir: Path,
-    *,
-    file_name: str = "db.sqlite3",
-    engine: str = "django.db.backends.sqlite3",
-    transaction_mode: TransactionMode = TransactionMode.IMMEDIATE,
-    timeout: int = 5,
-    init_command: str = SQLITE_INIT_COMMAND,
-):
-    config = {
-        "ENGINE": engine,
-        "NAME": base_dir / file_name,
-        "OPTIONS": {
-            "transaction_mode": str(transaction_mode),
-            "timeout": timeout,
-            "init_command": init_command,
-        },
-    }
-
-    return config
-
-
 __all__ = [
     sqlite_config,
     SQLITE_INIT_COMMAND,
+    Synchronous,
+    TempStore,
+    JournalMode,
+    TransactionMode,
 ]
---
+++ tests/configurator/test_sqlite_config.py
@@ -0,0 +1,174 @@
+import pytest
+from typeguard import TypeCheckError
+from dj_lite import (
+    sqlite_config,
+    TransactionMode,
+    JournalMode,
+    Synchronous,
+    SQLITE_INIT_COMMAND,
+)
+from pathlib import Path
+from copy import deepcopy
+
+
+default = {
+    "ENGINE": "django.db.backends.sqlite3",
+    "NAME": Path("db.sqlite3"),
+    "OPTIONS": {
+        "transaction_mode": "IMMEDIATE",
+        "timeout": 5,
+        "init_command": SQLITE_INIT_COMMAND,
+    },
+}
+
+
+def test_default():
+    expected = deepcopy(default)
+
+    actual = sqlite_config(Path("."))
+
+    assert actual == expected
+
+
+def test_file_name():
+    expected = deepcopy(default)
+    expected["NAME"] = Path("custom_db.sqlite3")
+
+    actual = sqlite_config(Path("."), file_name="custom_db.sqlite3")
+
+    assert actual == expected
+
+
+def test_engine():
+    expected = deepcopy(default)
+    expected["ENGINE"] = "custom.engine.path"
+
+    actual = sqlite_config(Path("."), engine="custom.engine.path")
+
+    assert actual == expected
+
+
+def test_transaction_mode_deferred():
+    expected = deepcopy(default)
+    expected["OPTIONS"]["transaction_mode"] = "DEFERRED"
+
+    actual = sqlite_config(Path("."), transaction_mode=TransactionMode.DEFERRED)
+
+    assert actual == expected
+
+
+def test_transaction_mode_exclusive():
+    expected = deepcopy(default)
+    expected["OPTIONS"]["transaction_mode"] = "EXCLUSIVE"
+
+    actual = sqlite_config(Path("."), transaction_mode=TransactionMode.EXCLUSIVE)
+
+    assert actual == expected
+
+
+def test_timeout():
+    expected = deepcopy(default)
+    expected["OPTIONS"]["timeout"] = 30
+
+    actual = sqlite_config(Path("."), timeout=30)
+
+    assert actual == expected
+
+
+def test_init_command():
+    init_command = """
+PRAGMA journal_mode=WAL;
+PRAGMA synchronous=FULL;"""
+
+    expected = deepcopy(default)
+    expected["OPTIONS"]["init_command"] = init_command
+
+    actual = sqlite_config(Path("."), init_command=init_command)
+
+    assert actual == expected
+
+
+def test_cache_size():
+    expected = deepcopy(default)
+    expected["OPTIONS"]["init_command"] = expected["OPTIONS"]["init_command"].replace(
+        "PRAGMA cache_size=2000", "PRAGMA cache_size=1"
+    )
+
+    actual = sqlite_config(Path("."), cache_size=1)
+
+    assert actual == expected
+
+
+def test_journal_mode():
+    expected = deepcopy(default)
+    expected["OPTIONS"]["init_command"] = expected["OPTIONS"]["init_command"].replace(
+        "PRAGMA journal_mode=WAL;", "PRAGMA journal_mode=DELETE;"
+    )
+
+    actual = sqlite_config(Path("."), journal_mode=JournalMode.DELETE)
+
+    assert actual == expected
+
+
+def test_synchronous():
+    expected = deepcopy(default)
+    expected["OPTIONS"]["init_command"] = expected["OPTIONS"]["init_command"].replace(
+        "PRAGMA synchronous=NORMAL;", "PRAGMA synchronous=FULL;"
+    )
+
+    actual = sqlite_config(Path("."), synchronous=Synchronous.FULL)
+
+    assert actual == expected
+
+
+def test_mmap_size():
+    expected = deepcopy(default)
+    expected["OPTIONS"]["init_command"] = expected["OPTIONS"]["init_command"].replace(
+        "PRAGMA mmap_size=134217728;", "PRAGMA mmap_size=268435456;"
+    )
+
+    actual = sqlite_config(Path("."), mmap_size=268435456)
+
+    assert actual == expected
+
+
+def test_journal_size_limit():
+    expected = deepcopy(default)
+    expected["OPTIONS"]["init_command"] = expected["OPTIONS"]["init_command"].replace(
+        "PRAGMA journal_size_limit=27103364;", "PRAGMA journal_size_limit=10000000;"
+    )
+
+    actual = sqlite_config(Path("."), journal_size_limit=10000000)
+
+    assert actual == expected
+
+
+def test_incorrect_type():
+    with pytest.raises(TypeCheckError) as e:
+        sqlite_config(Path("."), journal_size_limit="ohno")
+
+    assert (
+        e.exconly()
+        == 'typeguard.TypeCheckError: argument "journal_size_limit" (str) is not an instance of int'
+    )
+
+
+def test_extra_kwargs():
+    expected = deepcopy(default)
+    expected["OPTIONS"]["init_command"] = f"""PRAGMA something_extra=123;
+{expected["OPTIONS"]["init_command"]}"""
+
+    actual = sqlite_config(Path("."), pragmas={"something_extra": 123})
+
+    assert actual == expected
+
+
+def test_extra_kwargs_overrides():
+    expected = deepcopy(default)
+    expected["OPTIONS"]["init_command"] = expected["OPTIONS"]["init_command"].replace(
+        "PRAGMA journal_mode=WAL;", "PRAGMA journal_mode=OOPS;"
+    )
+
+    actual = sqlite_config(Path("."), pragmas={"journal_mode": "OOPS"})
+
+    assert actual == expected
